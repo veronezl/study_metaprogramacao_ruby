30392bfd1922754bc720bc1f6eeee0a28cfd7c0a
lucasveronez <lucasveronez.dev@gmail.com>
2024-08-12 21:31:41 -0300

    add: fourth commit

--- metaprogramacao/orm/init.rb
+++ metaprogramacao/orm/init.rb
@@ -1,12 +1,57 @@
 require_relative './models/produto'
+require_relative './models/fornecedor'
+require 'terminal-table'
 
-prod = Produto.new
+p = Produto.new
 
-puts "=== Métodos e atributos de intâncias ==="
+puts "=== metodos e atributos de instancia ==="
 puts p.methods - Class.methods
-puts "=== Métodos e atributos de classe ==="
+puts "=== metodos e atributos de classe ==="
 puts Produto.methods - Class.methods
 
-p.nome = 'Abacaxi'
+p.nome = "Abacaxi"
 puts p.inspect
 
+produtos = Produto.todos
+
+table = Terminal::Table.new do |t|
+  t.headings = ['ID', 'Nome', 'Descrição', 'Preço', 'Estoque', 'Fornecedor ID']
+  
+  # Preencha a tabela com os dados dos produtos
+  produtos.each do |produto|
+    t << [
+      produto.id,
+      produto.nome,
+      produto.descricao,
+      "%.2f" % produto.preco,  # Formata o preço para 2 casas decimais
+      produto.estoque,
+      produto.fornecedor_id
+    ]
+  end
+end
+
+# Imprime a tabela no console
+puts table
+
+
+
+puts "\n\n\n\n\n"
+
+
+fornecedores = Fornecedor.todos
+
+table = Terminal::Table.new do |t|
+  t.headings = ['ID', 'Nome', 'CNPJ']
+  
+  # Preencha a tabela com os dados dos produtos
+  fornecedores.each do |fornecedor|
+    t << [
+      fornecedor.id,
+      fornecedor.nome,
+      fornecedor.cnpj
+    ]
+  end
+end
+
+# Imprime a tabela no console
+puts table
--- metaprogramacao/orm/lib/json_orm.rb
+++ metaprogramacao/orm/lib/json_orm.rb
@@ -1,59 +1,64 @@
-require_relative 'json'
+require 'json'
 require 'byebug'
 
 module JsonOrm
   def self.included(base)
     base.extend(AtributosMetodosClasse)
-    base.extend(AtributosMetodosInstacia)
+    base.include(AtributosMetodosInstancia)
   end
 
   module AtributosMetodosClasse
-    
     def todos
       dados = ler_dados_json
-      objs = self.new
-    end 
+      objs = []
+      dados.each do |dado|
+        obj = self.new
+        dado.keys.each do |key|
+          obj.send("#{key}=", dado[key])
+        end
+        objs << obj
+      end
+
+      objs
+    end
+
+    protected
+
+    def arquivo_json(path_arquivo)
+      @path_arquivo = path_arquivo
+      define_atributos
+    end
     
-  protected
-
-  def arquivo_json(path_arquivo)
-    @path_arquivo = path_arquivo
-    define_atributos
-  end 
-
     private
 
     def ler_dados_json
-      if File.exist? (@path_arquivo)
+      if File.exist?(@path_arquivo)
         arquivo_json = File.read(@path_arquivo)
         return JSON.parse(arquivo_json)
-      end 
-       
-       []
-    end  
+      end
+
+      []
+    end
     
     def define_atributos
-      dosdos = ler_dados_json
+      dados = ler_dados_json
       atributos = dados.first.keys
-   
-        atributos.ench do |atributo|
-          define_method("#{atributo}=") do |value|
-            # eval("@#{atributo} = '#{value}")
-            instance_variable_set("@#{atributo}", value)
-          end
 
-          define_method("#{atributo}") do
-            # eval("return @#{atributo}")
-            instance_variable_get("@#{atributo}")
-          end
+      atributos.each do |atributo|
+        define_method("#{atributo}=") do |value|
+          instance_variable_set("@#{atributo}", value)
         end
+
+        define_method("#{atributo}") do
+          instance_variable_get("@#{atributo}")
+        end
+      end
     end
   end
 
   module AtributosMetodosInstancia
     def validar_nome
-      raise "Nome é obrigatório" if self.nome == nil || self.nome == "" # raise => dispara um erro
+      raise "Nome é obrigatório" if self.nome == nil || self.nome == ""
     end
   end
 end
-  
